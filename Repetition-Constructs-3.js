4b516a9623941fc90e8df472ef674513c256b111
Hemangi <hemangisabalpara11@gmail.com>
2024-01-14 22:33:26 +0530

    loop

---
+++ Repetition-Constructs-3.js
@@ -0,0 +1,41 @@
+//Repetition-Constructs-3
+
+// Many of the algorithms we study in this book are repetitive in nature. We use two
+// repetition constructs in this book—the while loop and the for loop.
+
+// The while loop
+
+
+var number = 1;
+var sum = 0;
+while (number < 11) {
+ sum += number;
+ ++number;
+}
+console.log(sum); // displays 55
+
+
+
+// When we want to execute a set of statements a specified number of times, we use a for
+// loop. Example uses a for loop to sum the integers 1 through 10.
+
+// Summing integers using a for loop
+
+var number = 1;
+var sum = 0;
+for (var number = 1; number < 11; number++) {
+ sum += number;
+}
+console.log(sum); // displays 55
+
+
+// for loops are also used frequently to access the elements of an array, as shown in
+// Example 
+// Using a for loop with an array
+
+var numbers = [3, 7, 12, 22, 100];
+var sum = 0;
+for (var i = 0; i < numbers.length; ++i) {
+ sum += numbers[i];
+}
+console.log(sum); // displays 144
